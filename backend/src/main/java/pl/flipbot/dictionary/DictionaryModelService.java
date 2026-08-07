cdff697624e6ca988e56078d9b8869e8c525b9f6
Jedrek <jedreek323@gmail.com>
2026-08-07 04:03:24 +0200

    poprawki serwisu modelu

--- backend/src/main/java/pl/flipbot/dictionary/DictionaryModelService.java
+++ backend/src/main/java/pl/flipbot/dictionary/DictionaryModelService.java
@@ -121,7 +121,7 @@ public class DictionaryModelService {
             Long brandId
     ) {
 
-        if(!dictionaryModelRepository.existsById(
+        if(!dictionaryBrandRepository.existsById(
                 brandId
         )) {
 
