11ee05e4d3c58d1b5a01766945252dbc1c6fcba9
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-14 23:09:28 +0200

    Carlisle: separate observer from regular bots

--- backend/src/main/java/pl/flipbot/bot/BotRepository.java
+++ backend/src/main/java/pl/flipbot/bot/BotRepository.java
@@ -3,6 +3,7 @@ package pl.flipbot.bot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BotRepository extends JpaRepository<Bot, Long> {
 
@@ -16,4 +17,12 @@ public interface BotRepository extends JpaRepository<Bot, Long> {
     List<Bot> findByStatus(
             BotStatus status
     );
+
+    List<Bot> findAllByMarketStatsObserverFalseOrderByIdAsc();
+
+    List<Bot> findByStatusAndMarketStatsObserverFalse(
+            BotStatus status
+    );
+
+    Optional<Bot> findFirstByMarketStatsObserverTrue();
 }
