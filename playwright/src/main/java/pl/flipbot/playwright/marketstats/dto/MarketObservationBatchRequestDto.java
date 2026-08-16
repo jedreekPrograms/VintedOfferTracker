157317c4859afaef505eebe07dbc386447977925
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-16 14:43:16 +0200

    send scanned price range with observations

--- playwright/src/main/java/pl/flipbot/playwright/marketstats/dto/MarketObservationBatchRequestDto.java
+++ playwright/src/main/java/pl/flipbot/playwright/marketstats/dto/MarketObservationBatchRequestDto.java
@@ -1,9 +1,12 @@
 package pl.flipbot.playwright.marketstats.dto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public record MarketObservationBatchRequestDto(
         List<String> listingIds,
-        boolean complete
+        boolean complete,
+        BigDecimal minPrice,
+        BigDecimal maxPrice
 ) {
 }
