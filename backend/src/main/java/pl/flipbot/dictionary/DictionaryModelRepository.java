b31fb3adb913760a4fa0ea2aa9b78b985e627d08
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-16 14:44:01 +0200

    lock model during observer range updates

--- backend/src/main/java/pl/flipbot/dictionary/DictionaryModelRepository.java
+++ backend/src/main/java/pl/flipbot/dictionary/DictionaryModelRepository.java
@@ -1,8 +1,13 @@
 package pl.flipbot.dictionary;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DictionaryModelRepository extends JpaRepository<DictionaryModel, Long> {
 
@@ -15,4 +20,13 @@ public interface DictionaryModelRepository extends JpaRepository<DictionaryModel
             Long brandId
     );
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+            select model
+            from DictionaryModel model
+            where model.id = :modelId
+            """)
+    Optional<DictionaryModel> findByIdForUpdate(
+            @Param("modelId") Long modelId
+    );
 }
