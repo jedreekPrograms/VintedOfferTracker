c5593e7e28e32f8458113750c4d966bca521ef29
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-18 09:41:50 +0200

    Load final UI detail overrides

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./styles/commercial-ui.css";
+import "./styles/commercial-ui-details.css";
 
 const rootElement = document.getElementById("root");
 
