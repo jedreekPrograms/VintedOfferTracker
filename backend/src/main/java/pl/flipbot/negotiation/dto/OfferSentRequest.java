037fb6a169f9e64f7e0e3a445bfe71da4e14d4cc
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-21 02:48:37 +0200

    Require bot id for offer-sent updates

--- backend/src/main/java/pl/flipbot/negotiation/dto/OfferSentRequest.java
+++ backend/src/main/java/pl/flipbot/negotiation/dto/OfferSentRequest.java
@@ -1,12 +1,16 @@
 package pl.flipbot.negotiation.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class OfferSentRequest {
+
+    @NotNull
+    private Long botId;
 
     @NotBlank
     private String listingId;
