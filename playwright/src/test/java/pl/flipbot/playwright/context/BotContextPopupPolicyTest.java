b676cae4a452481ece8c558362b322c52d24697a
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-26 23:56:09 +0200

    Remove popup-script regression expectation

--- playwright/src/test/java/pl/flipbot/playwright/context/BotContextPopupPolicyTest.java
+++ playwright/src/test/java/pl/flipbot/playwright/context/BotContextPopupPolicyTest.java
@@ -9,16 +9,6 @@ import static org.junit.Assert.assertTrue;
 public class BotContextPopupPolicyTest {
 
     @Test
-    public void preemptivePopupScriptBlocksWindowOpenAndBlankTargets() {
-        String script = BotContext.preemptivePopupSuppressionScript();
-
-        assertTrue(script.contains("window.open = () => null"));
-        assertTrue(script.contains("_blank"));
-        assertTrue(script.contains("document.addEventListener(\"click\""));
-        assertTrue(script.contains("document.addEventListener(\"submit\""));
-    }
-
-    @Test
     public void anonymousObserverUiScriptAcceptsOneTrustAndNormalizesModelRows() {
         String script = BotContext.anonymousObserverUiStabilityScript();
 
