ffa951e69b7d03639c11d8eebca4c58f15b9ae53
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-09-02 01:22:42 +0200

    Load premium UI layer after existing styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import "./styles/commercial-ui.css";
 import "./styles/commercial-ui-details.css";
 import "./styles/negotiation-response-policies.css";
+import "./styles/premium-ui.css";
 
 const rootElement = document.getElementById("root");
 
