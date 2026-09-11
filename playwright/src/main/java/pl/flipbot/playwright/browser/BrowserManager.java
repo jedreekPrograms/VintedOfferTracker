6cee8d6453b0d4296771af4cbea36e1a73e7e88e
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-09-11 19:31:53 +0200

    fix: install OneTrust consent guard

--- playwright/src/main/java/pl/flipbot/playwright/browser/BrowserManager.java
+++ playwright/src/main/java/pl/flipbot/playwright/browser/BrowserManager.java
@@ -95,9 +95,10 @@ public class BrowserManager implements AutoCloseable {
         }
 
         context.addInitScript(VintedInformationalDialogGuard.script());
+        context.addInitScript(OneTrustConsentGuard.script());
 
         log.debug(
-                "[BROWSER UI] Vinted informational-dialog guard installed for new browser context."
+                "[BROWSER UI] Vinted informational-dialog and OneTrust consent guards installed for new browser context."
         );
 
         return context;
