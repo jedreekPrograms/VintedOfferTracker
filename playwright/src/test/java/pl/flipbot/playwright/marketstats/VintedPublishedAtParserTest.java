6b6eba75e4044a02c475cdbcdb0d88744fdb41af
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-09-12 16:41:19 +0200

    test: cover singular Vinted year age

--- playwright/src/test/java/pl/flipbot/playwright/marketstats/VintedPublishedAtParserTest.java
+++ playwright/src/test/java/pl/flipbot/playwright/marketstats/VintedPublishedAtParserTest.java
@@ -57,6 +57,17 @@ public class VintedPublishedAtParserTest {
     }
 
     @Test
+    public void parsesConfirmedSingularVintedYearAge() {
+        assertEquals(
+                LocalDateTime.of(2025, 9, 11, 19, 40),
+                VintedPublishedAtParser.parse(
+                        "REL|roku",
+                        OBSERVED_AT
+                ).orElseThrow()
+        );
+    }
+
+    @Test
     public void convertsUtcTimestampToWarsawTime() {
         assertEquals(
                 LocalDateTime.of(2026, 9, 11, 12, 0),
