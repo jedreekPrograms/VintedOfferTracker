fd60844d5fad7ff2c9bbd4c33229e22c93eef3e4
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-09-14 12:48:06 +0200

    test: require cascading additional-target foreign keys

---
+++ backend/src/test/java/pl/flipbot/bot/configuration/BotAdditionalTargetForeignKeyTest.java
@@ -0,0 +1,39 @@
+package pl.flipbot.bot.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class BotAdditionalTargetForeignKeyTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void targetOwnedRowsCascadeWhenAdditionalTargetIsDeleted() {
+        assertCascade("fk_negotiation_step_additional_target");
+        assertCascade("fk_listing_additional_target");
+    }
+
+    private void assertCascade(String constraintName) {
+        String deleteAction = jdbcTemplate.queryForObject(
+                """
+                SELECT confdeltype::text
+                FROM pg_constraint
+                WHERE conname = ?
+                """,
+                String.class,
+                constraintName
+        );
+
+        assertEquals(
+                "c",
+                deleteAction,
+                () -> constraintName + " must use ON DELETE CASCADE"
+        );
+    }
+}
