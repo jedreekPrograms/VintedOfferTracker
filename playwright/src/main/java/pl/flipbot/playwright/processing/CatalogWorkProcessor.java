7f1e0ce29ad91957d505169fcf3a2cd7f79ea1b3
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-09-15 12:46:56 +0200

    testability: expose catalog rotation seed to package tests

--- playwright/src/main/java/pl/flipbot/playwright/processing/CatalogWorkProcessor.java
+++ playwright/src/main/java/pl/flipbot/playwright/processing/CatalogWorkProcessor.java
@@ -152,6 +152,10 @@ public class CatalogWorkProcessor {
         }
     }
 
+    static void setRotationStateForTests(Long botId, int offset) {
+        NEXT_PRODUCT_OFFSET.put(botId, offset);
+    }
+
     static boolean hasRotationStateForTests(Long botId) {
         return botId != null && NEXT_PRODUCT_OFFSET.containsKey(botId);
     }
