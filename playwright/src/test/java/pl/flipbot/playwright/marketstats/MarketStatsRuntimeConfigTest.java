9369b63c5c617b96de5effb552e5ae896ef2b90f
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-09-15 12:49:53 +0200

    test: cover market-stats browser recycle configuration

--- playwright/src/test/java/pl/flipbot/playwright/marketstats/MarketStatsRuntimeConfigTest.java
+++ playwright/src/test/java/pl/flipbot/playwright/marketstats/MarketStatsRuntimeConfigTest.java
@@ -121,4 +121,32 @@ public class MarketStatsRuntimeConfigTest {
                 )
         );
     }
+
+    @Test
+    public void marketStatsBrowserDefaultsToThreeTargetsBeforeRecycle() {
+        assertEquals(
+                3,
+                MarketStatsRuntimeConfig.resolveBrowserRecycleTargetCount(null)
+        );
+        assertEquals(
+                3,
+                MarketStatsRuntimeConfig.resolveBrowserRecycleTargetCount("bad")
+        );
+    }
+
+    @Test
+    public void marketStatsBrowserRecycleCountIsConfigurableAndBounded() {
+        assertEquals(
+                5,
+                MarketStatsRuntimeConfig.resolveBrowserRecycleTargetCount("5")
+        );
+        assertEquals(
+                1,
+                MarketStatsRuntimeConfig.resolveBrowserRecycleTargetCount("0")
+        );
+        assertEquals(
+                50,
+                MarketStatsRuntimeConfig.resolveBrowserRecycleTargetCount("999")
+        );
+    }
 }
