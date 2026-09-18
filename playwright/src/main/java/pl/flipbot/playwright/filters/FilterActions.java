f1a35374c4782a52cbb42cdcca4e7fcf7ffddee2
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-09-18 22:36:41 +0200

    fix: restore reload wait-state import

--- playwright/src/main/java/pl/flipbot/playwright/filters/FilterActions.java
+++ playwright/src/main/java/pl/flipbot/playwright/filters/FilterActions.java
@@ -4,6 +4,7 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.WaitForSelectorState;
+import com.microsoft.playwright.options.WaitUntilState;
 import lombok.extern.slf4j.Slf4j;
 import pl.flipbot.playwright.context.BotContext;
 import pl.flipbot.playwright.marketplace.MarketplaceNavigator;
